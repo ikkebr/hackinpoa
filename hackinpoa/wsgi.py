747ce83bbd90af1c51983d041353a979c21d76a9
Henrique Pereira <ikkibr@gmail.com>
2015-04-09 17:52:00 -0300

    Better coverage

--- hackinpoa/wsgi.py
+++ hackinpoa/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
+#pragma: no cover
 import os
 
 from django.core.wsgi import get_wsgi_application
