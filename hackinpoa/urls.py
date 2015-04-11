927db7c7d112d5d3ddca60e79a9d5a9983967293
Henrique Pereira <ikkibr@gmail.com>
2015-04-11 12:36:25 -0300

    added django-allauth

--- hackinpoa/urls.py
+++ hackinpoa/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^', include('base.urls')),
+    
+    url(r'^accounts/', include('allauth.urls')),
+    
     url(r'^login', 'django.contrib.auth.views.login',
         {'template_name': 'base/login.html'}, name="login"),
     url(r'^logout', 'django.contrib.auth.views.logout_then_login',
