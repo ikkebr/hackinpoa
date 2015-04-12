f6ea8e4d0cab35af723f709daee0a1241687f375
Fabiano Moraes <fabiano.moraes@outlook.com>
2015-04-12 02:50:02 -0300

    =COMMIIIIIT 101git add .git add .git add .git add .git add .git add .git add .

--- trip/views.py
+++ trip/views.py
@@ -14,6 +14,7 @@ class CreateTrip(LoginRequiredMixin, CreateView):
 
     model = Trip
     template_name = "trip/create.html"
+    form = TripForm
 
 
 class CreateRoute(LoginRequiredMixin, CreateView):
